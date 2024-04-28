5679df95689c6c7eb7eaccef1e64a90250518e7a
abu salah <abu salah@POTATOLEGEND>
2024-04-28 19:42:07 +0300

    comit

---
+++ AppliancePalace/Entities/ShoppingCart.cs
@@ -0,0 +1,11 @@
+﻿using NUlid;
+
+namespace AppliancePalace;
+
+public class ShoppingCart : BaseEntity
+{
+    public ICollection<Product> Products { get; set; } = null!;
+    public int Quantity { get; set; }
+    public Ulid UserId { get; set; }
+    public User User { get; set; } = null!;
+}
